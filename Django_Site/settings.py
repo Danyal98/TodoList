25ad61f3ad3066babd273454cda9eabb0c61b13f
Daniyal Akram <daniyalakram94@.com>
2022-04-12 22:09:49 +0500

    npm version

--- Django_Site/settings.py
+++ Django_Site/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'todofrontend/build/static') 
+    os.path.join(BASE_DIR, 'build/static') 
 ]
 
 # Default primary key field type
