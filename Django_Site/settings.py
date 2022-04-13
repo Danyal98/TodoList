a6e45cdade18b10fd29d1d152e62f6943555cf9e
Daniyal Akram <daniyalakram94@.com>
2022-04-13 21:38:51 +0500

    manifest

--- Django_Site/settings.py
+++ Django_Site/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!&qg&n*&-l+&*zbk*bbyh&1mcz_e@x0k3x^(r&9j42c*#(f=bv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static') 
+    os.path.join(BASE_DIR, 'todofrontend/build/static') 
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
